c450bf99b9c2da1f2d880fa991c7a9b822b6941b
rajputanuj31 <rajputanuj041@gmail.com>
2023-12-10 06:11:57 +0530

    Added Abi's

---
+++ frontend/increamentABI.js
@@ -0,0 +1,2 @@
+const abi = [{"inputs":[],"name":"getValue","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"increament","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+export default abi;
